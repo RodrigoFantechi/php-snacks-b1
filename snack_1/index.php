4d0ebc7bddbe7ee9ccbc7c9dabe505a30003e260
RodrigoFantechi <rodrifante595@libero.it>
2022-11-28 12:09:13 +0100

    finish snack 1

---
+++ snack_1/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*  ##Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+*/
+
+$partite =[
+    ['Lackers', 'celtics', 80, 75],
+    ['Lackers', 'celtics', 80, 75],
+    ['Lackers', 'celtics', 80, 75],
+    ['Lackers', 'celtics', 80, 75],
+];
+
+foreach ($partite as  $partita) {
+    foreach ($partita as $key => $detail){
+        echo $detail . ' ';
+        if ($key == count($partita)-1){
+        echo '<br>';
+        }
+
+    }
+}
+
+?>
