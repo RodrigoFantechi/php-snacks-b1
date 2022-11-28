2d87fc854f9ae1544715e87880f092875bd66b33
RodrigoFantechi <rodrifante595@libero.it>
2022-11-28 12:17:29 +0100

    fix name basketball teams

--- snack_1/index.php
+++ snack_1/index.php
@@ -8,8 +8,8 @@ Olimpia Milano - Cantù | 55-60
 $partite =[
     ['Lackers', 'celtics', 80, 75],
     ['Clippers', 'Miami Heats', 102, 82],
-    ['Knight', 'Bulls', 97, 102],
-    ['wizzard', 'warriors', 98, 62],
+    ['King', 'Bulls', 97, 102],
+    ['Wizzard', 'Warriors', 98, 62],
 ];
 
 foreach ($partite as  $partita) {
